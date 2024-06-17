75e7ce79b1c3b6ebd9abc966fc86a3201bd84db9
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-06-17 09:23:30 -0500

    menu interfaz

--- Gestion de notas/Gestion de notas/Menu.Designer.cs
+++ Gestion de notas/Gestion de notas/Menu.Designer.cs
@@ -29,15 +29,57 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gestionEstudiantesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDocentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gradosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.materiasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionEstudiantesToolStripMenuItem,
+            this.gestionDocentesToolStripMenuItem,
+            this.gradosToolStripMenuItem,
+            this.materiasToolStripMenuItem,
+            this.notasToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gestionEstudiantesToolStripMenuItem
+            // 
+            this.gestionEstudiantesToolStripMenuItem.Name = "gestionEstudiantesToolStripMenuItem";
+            this.gestionEstudiantesToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.gestionEstudiantesToolStripMenuItem.Text = "Gestion Estudiantes";
+            // 
+            // gestionDocentesToolStripMenuItem
+            // 
+            this.gestionDocentesToolStripMenuItem.Name = "gestionDocentesToolStripMenuItem";
+            this.gestionDocentesToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.gestionDocentesToolStripMenuItem.Text = "Gestion Docentes";
+            // 
+            // gradosToolStripMenuItem
+            // 
+            this.gradosToolStripMenuItem.Name = "gradosToolStripMenuItem";
+            this.gradosToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.gradosToolStripMenuItem.Text = "Grados";
+            // 
+            // materiasToolStripMenuItem
+            // 
+            this.materiasToolStripMenuItem.Name = "materiasToolStripMenuItem";
+            this.materiasToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.materiasToolStripMenuItem.Text = "Materias";
+            // 
+            // notasToolStripMenuItem
+            // 
+            this.notasToolStripMenuItem.Name = "notasToolStripMenuItem";
+            this.notasToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.notasToolStripMenuItem.Text = "Notas";
             // 
             // Menu
             // 
@@ -48,6 +90,8 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
             this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +100,10 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gestionEstudiantesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionDocentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gradosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem materiasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem notasToolStripMenuItem;
     }
 }
